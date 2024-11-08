88a94e8f953476f726bac96175f1d58ffa287dc8
Avinash7390 <avinashk82495@gmail.com>
2024-11-09 02:31:02 +0530

    Team Page Enhancement

--- client/src/Components/Team/PersonCard.jsx
+++ client/src/Components/Team/PersonCard.jsx
@@ -1,15 +1,24 @@
-
 function PersonCard({ profile }) {
-    return (
-        <div className="w-full border-gray-200 border-2 h-full flex flex-col">
-            <div className="w-full h-[90%]">
-                {profile.img}
-            </div>
-            <div className="h-[10%] text-center 
-            text-[1rem] md:text-[1rem] lg:text-[1.5vw] text-white font-bebas font-normal">
-                {profile.name}
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full border-gray-200 border-2 h-full flex flex-col">
+      <div className="flex w-full h-[90%] justify-center">
+        {/* <img
+          src={profile?.img}
+          style={{
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+          className="object-fit"
+        /> */}
+      </div>
+      <div
+        className="h-[10%] text-center 
+            text-[1rem] md:text-[1rem] lg:text-[1.5vw] text-white font-bebas font-normal"
+      >
+        {profile.name}
+      </div>
+    </div>
+  );
 }
 export default PersonCard;
